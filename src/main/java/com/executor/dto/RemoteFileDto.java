bf484a94a072a872dea2a41b71ecb5497ec98993
T5uy0 <julienschneider964@gmail.com>
2026-03-30 12:26:17 +0200

    refactor: delete unused method.

--- src/main/java/com/executor/dto/RemoteFileDto.java
+++ src/main/java/com/executor/dto/RemoteFileDto.java
@@ -6,36 +6,14 @@ public class RemoteFileDto {
     private String fileName;
     private long size;
 
-    public RemoteFileDto() {
-    }
-
     public RemoteFileDto(String key, String fileName, long size) {
         this.key = key;
         this.fileName = fileName;
         this.size = size;
     }
 
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
     public String getFileName() {
         return fileName;
     }
 
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
-    }
 }
