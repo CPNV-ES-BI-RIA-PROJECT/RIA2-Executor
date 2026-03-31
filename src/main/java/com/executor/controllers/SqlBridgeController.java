d494b55ce6084ab1db7c2585495b2591ae97e28d
T5uy0 <julienschneider964@gmail.com>
2026-03-31 08:37:49 +0200

    fix: global import

--- src/main/java/com/executor/controllers/SqlBridgeController.java
+++ src/main/java/com/executor/controllers/SqlBridgeController.java
@@ -6,7 +6,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
